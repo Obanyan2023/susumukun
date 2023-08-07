21aa02ee2b52fb7919ce8ad5714f5eb0e14bec3f
e1b21074 <118655498+e1b21074@users.noreply.github.com>
2023-08-07 18:38:28 +0900

    menubutton

--- frontend/src/components/Layout/Menu.tsx
+++ frontend/src/components/Layout/Menu.tsx
@@ -1,7 +1,12 @@
 import { MainLayout } from "./MainLayout";
+import * as React from "react";
+import { Button } from "@mui/material";
 
 export const Menu = () => (
     <MainLayout >
         <h1>Menu</h1>
+        <Button>text</Button>
+        <Button variant="contained">contained</Button>
+        <Button variant="outlined">outlined</Button>
     </MainLayout>
 )
