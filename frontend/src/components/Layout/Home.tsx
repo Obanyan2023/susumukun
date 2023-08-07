42470fede290ff7829b45a39fe7951f81f331ae6
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2023-08-07 21:22:48 +0900

    feat: welcomeページの土台作成

--- frontend/src/components/Layout/Home.tsx
+++ frontend/src/components/Layout/Home.tsx
@@ -1,10 +1,28 @@
-import { Grid } from "@mui/material";
 import { MainLayout } from "./MainLayout";
+import {
+    Button,
+    Grid,
+    Typography
+} from "@mui/material";
 
 export const Home = () => (
-    <MainLayout title={"welcome"}>
-        <Grid container alignItems={"center"} justifyContent={"center"} direction={"column"}>
-            <h1>Hello</h1>
+    <MainLayout title={"アプリ探検隊と行くフラット開発ベアー - ようこそ！"}>
+        <Grid container alignItems={"center"} direction={"column"}>
+            <Grid item m={15}>
+                <Typography variant="h2">
+                    {"アプリ探検隊と行くフラット開発ベアー"}
+                </Typography>
+            </Grid>
+            <Grid item>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    href="/menu"
+                >メニュー</Button>
+            </Grid>
+            <Grid item m={15} border={1}>
+                {"ゲーム説明"}
+            </Grid>
         </Grid>
     </MainLayout>
 )
