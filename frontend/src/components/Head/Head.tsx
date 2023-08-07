8b4232a93631b8061ab7e5b86d0066134d28b4ad
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2023-08-07 18:07:18 +0900

    feat: webページのタイトル（ヘッダー）部分を編集できるように変更

--- frontend/src/components/Head/Head.tsx
+++ frontend/src/components/Head/Head.tsx
@@ -1,6 +1,20 @@
 import "../../App.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
+type HeadProps = {
+    title: string
+};
 
 /**
+ * @param {string} title - タイトル
  * @returns {JSX.Element} - ヘッダー(もどき)
  */
-export const Head = () => <></>
+export const Head = ({ title }: HeadProps) => {
+    return (
+        <HelmetProvider>
+            <Helmet>
+                <title>{title}</title>
+            </Helmet>
+        </HelmetProvider>
+    );
+}
