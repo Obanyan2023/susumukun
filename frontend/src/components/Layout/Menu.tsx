d6c9567093481fef486cb1a6b97b4eaf317ea39e
e1b21074 <118655498+e1b21074@users.noreply.github.com>
2023-08-09 16:02:09 +0900

    menu_change

--- frontend/src/components/Layout/Menu.tsx
+++ frontend/src/components/Layout/Menu.tsx
@@ -1,12 +1,20 @@
 import { MainLayout } from "./MainLayout";
 import * as React from "react";
-import { Button } from "@mui/material";
+import { Button, TypeBackground } from "@mui/material";
 
 export const Menu = () => (
-    <MainLayout >
-        <h1>Menu</h1>
-        <Button>text</Button>
-        <Button variant="contained">contained</Button>
-        <Button variant="outlined">outlined</Button>
+    <MainLayout>
+        <h1>メニュー画面</h1>
+        <body>
+            <section>
+                <Button variant="contained">ゲームスタート</Button>        
+            </section>
+            <section>
+                <Button variant="outlined">ルール説明</Button>        
+            </section>
+            <section>
+                <Button variant="outlined">スコア確認</Button>
+            </section>
+        </body>
     </MainLayout>
 )
