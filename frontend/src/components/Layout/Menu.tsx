13da211dd08141bd0f0834833d5f4e823e2b3d59
naoya <81454816+naoya0117@users.noreply.github.com>
2023-08-21 15:46:53 +0900

    adjust menu layoutw

--- frontend/src/components/Layout/Menu.tsx
+++ frontend/src/components/Layout/Menu.tsx
@@ -1,20 +1,16 @@
 import { MainLayout } from "./MainLayout";
 import * as React from "react";
-import { Button, TypeBackground } from "@mui/material";
+import { Button, Grid, TypeBackground } from "@mui/material";
 
 export const Menu = () => (
     <MainLayout title={"Menu"}>
-        <h1>メニュー画面</h1>
-        <body>
-            <section>
-                <Button variant="contained">ゲームスタート</Button>        
-            </section>
-            <section>
-                <Button variant="outlined">ルール説明</Button>        
-            </section>
-            <section>
-                <Button variant="outlined">スコア確認</Button>
-            </section>
-        </body>
+
+        <Grid container alignItems="center" justifyContent="center" direction="column">
+            <h1>メニュー画面</h1>
+            <Button variant="contained" sx={{margin:3}}>ゲームスタート</Button>        
+            <Button variant="outlined" sx={{margin:3}}>ルール説明</Button>        
+            <Button variant="outlined" sx={{margin:3}}> スコア確認</Button>
+        </Grid>
+
     </MainLayout>
 )
