c585aa777c590aaccd9415bc47c5d8f33a5684c9
e1b21115 <e1b21115@oit.ac.jp>
2023-09-29 11:36:59 +0900

    一旦階段まで

--- frontend/src/features/game/stages/main/ground/Platform.ts
+++ frontend/src/features/game/stages/main/ground/Platform.ts
@@ -25,21 +25,46 @@ export default class Platform extends BaseGround {
      */
     create(): void {
         let x;
-        const bottom = window.innerHeight-30;
+        let end;
+        const bottom = window.innerHeight - 30;
+        const blocksize = 64;
         this.object = this.scene.physics.add.staticGroup();
-        
-        for (x = 16; x < window.innerWidth; x += 32) {
-            this.object
-                .create(x, bottom, "ground")
-                .setScale(2)
-                .refreshBody();
-        }
-        x += 32;
-        this.object.create(x, bottom-64, "ground").setScale(2).refreshBody();
-        x += 64;
-        this.object.create(x, bottom-128, "ground").setScale(2).refreshBody();
-        x += 64;
-        this.object.create(x, bottom-192, "ground").setScale(2).refreshBody();
 
+        for (x = 16; x < window.innerWidth * 3; x += blocksize) {//全区画の1番下の地面
+            console.log(x);
+            this.setBlock(x, bottom);
+        }
+        x = window.innerWidth + blocksize;
+        console.log(x);
+        this.setBlock(x, bottom - blocksize * 2);//2区画目左側のブロック
+        x += blocksize * 3;
+        for (let i = x; i < x + blocksize * 3; i += blocksize) {//2区画目右側のブロック3つ
+            console.log("i" + i);
+            this.setBlock(i, bottom - blocksize * 2);
+        }
+        x = window.innerWidth * 2 + window.innerWidth / 6;
+        console.log(x);
+        end = x + blocksize * 3;
+        for (let i = bottom - blocksize; i >= bottom - blocksize * 3; i -= blocksize) {//3区画目の登り階段
+            console.log("ii" + i);
+            for (let j = x; j < end; j += blocksize) {
+                console.log("j" + j);
+                this.setBlock(j, i);
+            }
+            x += blocksize;
+        }
+    }
+    /**
+     * ブロックの設置
+     * 
+     * @param {number: x, number: y} ブロックの座標
+     * @returns {void} 戻り値なし
+    */
+    setBlock(x: number, y: number): void {
+        if (this.object === null) {
+            return;
+        }
+
+        this.object.create(x, y, "ground").setScale(2).refreshBody();
     }
 }
