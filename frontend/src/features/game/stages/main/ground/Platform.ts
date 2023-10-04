c728638448a7a33ea593fab9c46c916ae522d60a
e1b21115 <e1b21115@oit.ac.jp>
2023-10-04 11:41:37 +0900

    現時点で作成している地面の調整

--- frontend/src/features/game/stages/main/ground/Platform.ts
+++ frontend/src/features/game/stages/main/ground/Platform.ts
@@ -30,14 +30,15 @@ export default class Platform extends BaseGround {
         const blocksize = 32;
         this.object = this.scene.physics.add.staticGroup();
 
-        for (x = 16; x < window.innerWidth + blocksize *20; x += blocksize) {//全区画の1番下の地面
+        for (x = 0; x <= window.innerWidth + blocksize *20; x += blocksize) {//全区画の1番下の地面
             console.log(x);
             this.setBlock(x, bottom);
+            this.setBlock(x, bottom+blocksize);
         }
-        this.setBlock(x-24, bottom);
         for (x = window.innerWidth + blocksize * 23; x < window.innerWidth * 3; x += blocksize) {//全区画の1番下の地面
             console.log(x);
             this.setBlock(x, bottom);
+            this.setBlock(x, bottom+blocksize);
         }
         x = window.innerWidth - blocksize * 10;
         console.log(x);
