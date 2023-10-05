0a3bf1a856689f02a7089456ee68e5f9ff8ce16d
e1b21115 <e1b21115@oit.ac.jp>
2023-10-05 18:53:20 +0900

    透明ブロックの設置をsetblockメソッドに組み込んだ

--- frontend/src/features/game/scenes/MainScene.ts
+++ frontend/src/features/game/scenes/MainScene.ts
@@ -132,12 +132,12 @@ export default class MainScene extends Phaser.Scene {
      */
     update(): void {
         // プレイヤー落下時にゲームオーバー画面に遷移する
-        const playerHeight : number = 17
+        const playerHeight: number = 17
         if (!this.physics.world.bounds.contains(this.cameras.main.width / 2, (this.player.object?.y as number) + playerHeight)) {
             this.player.destroy(
-               () => {
+                () => {
                     this.scene.start("GameOver");
-               } 
+                }
                 , 1000);
         }
         this.player.callLimitVelocityX(-160, 160);
