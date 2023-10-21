dc926eb14b60a45ff79a731a6fbe25f16b2ed11b
sho037 <103867061+sho037@users.noreply.github.com>
2023-10-21 11:31:04 +0900

    コンフリクト解消

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -11,9 +11,10 @@ import {
 } from "@mui/material";
 import * as React from 'react';
 import Background from '../assets/images/image.png';
-import '../index.css'
-import { useState } from 'react';
-import {GameComponent} from "../components/Game";
+import '../index.css';
+import { GameComponent } from "../components/Game";
+import Select from '@mui/material/Select';
+import { SelectChangeEvent } from '@mui/material/Select/SelectInput';
 
 const style = {
     position: 'absolute' as 'absolute',
@@ -56,7 +57,8 @@ const head = () => {
 };
 
 export const Home = () => {
-    const [ isFullScreen, setIsFullScreen ] = React.useState<boolean>(document.fullscreenElement ? true : false);
+    const [isFullScreen, setIsFullScreen] = React.useState<boolean>(document.fullscreenElement ? true : false);
+    const [nickname, setNickname] = React.useState<string>("");
     const [open, setOpen] = React.useState(false);
 
     const [difficult, setDifficult] = React.useState<number>(2);
@@ -85,50 +87,62 @@ export const Home = () => {
     }
 
     const HomeComponent = () => (
-            <Box sx={image}>
-                <Grid container alignItems={"center"} direction={"column"} sx={{ overflow: 'hidden', height: '100vh', width: '100vw' }}>
-                    <Grid container spacing={2} sx={{ height: "30%" }} alignItems="center">
-                        <Grid item xs={12}>
-                            <Typography className="mfont" align="center" sx={{ fontFamily: 'Mochiy Pop P One', fontSize: 50 }}>
-                                {"走れ！すすむ君！"}
-                            </Typography>
-                        </Grid>
-                    </Grid>
-                    <Grid container alignItems="center" direction="column" sx={{ bottom: "10%" }} >
-                        <Button variant="contained" sx={{ margin: 3 }} onClick={() => handleGameStart()}>ゲームスタート</Button>
-                        <Button variant="contained" color='inherit' sx={{ margin: 3 }} onClick={handleOpen}>ルール説明</Button>
-                        <Modal
-                            open={open}
-                            onClose={handleClose}
-                            aria-labelledby="modal-modal-title"
-                            aria-describedby="modal-modal-description"
-                        >
-                            <Box sx={style}>
-                                <Typography id="modal-modal-title" variant="h6" component="h2">
-                                    ルール説明
-                                </Typography>
-                                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                                    ・三回ミスしたらゲームオーバー<br />
-                                    ・ステージが変わるごとに体力は回復する<br />
-                                    ・スコアを他のプレイヤーと競う<br />
-                                    ・最高得点が10000 <br />
-                                    ・アイテム：100点（隠しアイテムは点数を高くする）<br />
-                                    ・敵：100点～（敵が強くなるたびに加算点数を増加させるから最高得点未定！敵の数によって決める) <br />
-                                </Typography>
-                                <Box sx={closebutton}>
-                                    <Button variant="contained" onClick={handleClose}>閉じる</Button>
-                                </Box>
-                            </Box>
-                        </Modal>
-                        <Button href="/scores" variant="contained" color='inherit' sx={{ margin: 3 }}> スコア確認</Button>
+        <Box sx={image}>
+            <Grid container alignItems={"center"} direction={"column"} sx={{ overflow: 'hidden', height: '100vh', width: '100vw' }}>
+                <Grid container spacing={2} sx={{ height: "30%" }} alignItems="center">
+                    <Grid item xs={12}>
+                        <Typography className="mfont" align="center" sx={{ fontFamily: 'Mochiy Pop P One', fontSize: 50 }}>
+                            {"走れ！すすむ君！"}
+                        </Typography>
                     </Grid>
                 </Grid>
-            </Box>
+                <Grid container alignItems="center" direction="column" sx={{ bottom: "10%" }} >
+                    <Typography color="#000000" bgcolor="#ffffff">ニックネーム入力</Typography>
+                    <input value={nickname} onChange={(e) => setNickname(e.target.value)} /><br />
+                    <FormControl sx={{ background: "#ffffff", width: 100, marginRight: '10px', borderRadius: "5px" }}>
+                        <InputLabel id="a-label">難易度</InputLabel>
+                        <Select labelId="a-label" id="a" onChange={handleChange} value={difficult} label="Age">
+                            <MenuItem value={1}>Hard</MenuItem>
+                            <MenuItem value={2}>Normal</MenuItem>
+                            <MenuItem value={4}>Easy</MenuItem>
+                        </Select>
+                    </FormControl><br />
+                    <Button variant="contained" onClick={() => handleGameStart(difficult)}>Game Start</Button><br />
+                    <Button variant="contained" color='inherit' sx={{ margin: 3 }} onClick={handleOpen}>ゲーム内容</Button>
+                    <Modal
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                    >
+                        <Box sx={style}>
+                            <Typography id="modal-modal-title" variant="h6" component="h2">
+                                ゲーム内容
+                            </Typography>
+                            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                            <b>「走れ！すすむ君！」はアプリ開発の世界を舞台にしたゲームです。主人公、すすむくんはアプリ開発者で、新しいアプリを完成させるためにはバグ（昆虫）たちとの戦いを繰り広げなければなりません。</b><br/>
+                            <br/>
+                            <strong>主人公のすすむ君を動かしてゴールを目指すゲーム！<br/></strong>
+                                ・緑のいもむしをふむと50点！<br/>
+                                ・赤のいもむしをふむと100点！<br/>
+                                ・エラーが出ているいもむしをふむと130点！<br/>
+                                ・バッタを踏むと150点！<br/>
+                                ・ゲームオーバーやタイムオーバーになると点数が減ってしまうゾ！<br/>
+                            </Typography>
+                            <Box sx={closebutton}>
+                                <Button variant="contained" onClick={handleClose}>閉じる</Button>
+                            </Box>
+                        </Box>
+                    </Modal>
+                    <Button href="/scores" variant="contained" color='inherit' sx={{ margin: 3 }}> スコア確認</Button>
+                </Grid >
+            </Grid >
+        </Box>
     );
 
     return (
         <MainLayout title={"走れ！すすむ君！ - " + isFullScreen ? "ようこそ！" : "ゲーム"} head={head()}>
-            {isFullScreen ? <GameComponent/> : <HomeComponent/>}
+            {isFullScreen ? <GameComponent /> : HomeComponent()}
         </MainLayout>
     )
 }
