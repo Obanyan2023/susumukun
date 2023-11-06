e570ce2b40574ab9343433f039a3e48e75f7caae
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2023-11-07 01:31:26 +0900

    feat: 横画面化処理に失敗した際にユーザに伝える #114

--- frontend/src/features/game/constants/localStorageKeys.ts
+++ frontend/src/features/game/constants/localStorageKeys.ts
@@ -9,3 +9,8 @@ export const CAN_CHALLENGE = "canChallenge";
 
 // チャレンジモード解禁時の通知用フラグを保存するためのキー
 export const CHA_CHALLENGE_NOTIFICATION = "chaChallengeNotification";
+
+// 画面の向きを固定するためのフラグを保存するためのキー
+export const TRY_ORIENTATION_LOCK = "tryOrientationLock";
+
+export const PLAYING = "playing";
